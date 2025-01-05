b16af79293f14a14892d5eddcb7bc0d2aa6f4ba2
Ujjawal Verma <ujjawalverma221@gmail.com>
2025-01-06 00:16:55 +0530

    expressRouter() is now in use

---
+++ routes/courses.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const CourseRouter = express.Router();
+
+    CourseRouter.post('/course/purchase',(req,res)=>{
+
+    })
+
+    CourseRouter.get('/course',(req,res)=>{
+
+    })
+
+module.exports = {
+    CourseRouter
+}
---
+++ routes/user.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const UserRouter = express.Router();
+
+    UserRouter.post('/user/signup',(req,res)=>{
+
+    })
+    
+    UserRouter.post('/user/signin',(req,res)=>{
+    
+    })
+module.exports = {
+    UserRouter
+}
